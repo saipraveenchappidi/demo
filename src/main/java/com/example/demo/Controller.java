ebef6e91bb12d9f00d7b03bf80502ef56db859d3
saipraveenchappidi <praveen@sais-MacBook-Air.local>
2026-06-16 12:06:08 +0530

    controller converted to test

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -7,35 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class Controller {
     @RequestMapping("/test")
-    public Student hello() {
+    public String hello() {
 
         System.out.println("call received for ramesh kumar testing api");
-        return new Student("Ramesh Kumar", 30);
-    }
-}
-
-class Student {
-    private String name;
-    private int age;
-
-    public Student(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
+        return "test api string response";
     }
 }
