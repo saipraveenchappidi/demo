a20f2f4963cca665a67892d174d2f9de2dd12a1e
saipraveenchappidi <praveen@sais-MacBook-Air.local>
2026-06-16 12:00:10 +0530

    first project commit

---
+++ src/main/java/com/example/demo/StartupRunner.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StartupRunner implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) {
+        System.out.println("Application Started .... Testting   ######");
+    }
+}
